38c4f7e4524ce124efb2e7d09fee15e354ef4779
wshino <wshinohara@gmail.com>
2019-04-10 22:34:08 +0900

    dockerfile for deploy contracts

---
+++ deploy-contracts/wait.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+#npm i zmq
+
+echo "Waiting for ganache-cli started."
+until curl -s -H "Content-Type: application/json" -X POST --data \
+    '{"id":1337,"jsonrpc":"2.0","method":"evm_snapshot","params":[]}' \
+    http://docker.for.mac.host.internal:8545 1> /dev/null;
+do
+    >&2 echo -n "."
+    sleep 1
+done
+
+echo "ganache-cli is up."
+
+cd packages/contracts && truffle migrate --network dockercompose
+#truffle migrate --network $DEPLOYMENT_TARGET
+echo "contract has been deployed."
