7046b83e9904318b2a8d82517ee797695c5679e3
wshino <wshinohara@gmail.com>
2019-04-11 19:14:29 +0900

    add operator for docker-compose

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,10 @@ MAINTAINER Cryptoeconomics Lab <https://www.cryptoeconomicslab.com>
 ENV DB_BASEPATH=/var/plasmadb
 ENV NPM_CONFIG_PREFIX=/home/node/.npm-global
 
-RUN apk update && apk add python make g++
+RUN apk update && apk add python make g++ curl jq
 USER node
 RUN npm install @layer2/operator -g
+
+COPY wait.sh wait.sh
 
 ENTRYPOINT ["/home/node/.npm-global/bin/layer2-operator"]
--- wait.sh
+++ wait.sh
@@ -3,15 +3,19 @@
 set -e
 cmd="$@"
 
+# not deployed -> result 0x
+# deployed -> result 0x........
 echo "Waiting for contract deployed to ganache-cli."
-until curl -s -H "Content-Type: application/json" -X POST --data \
+while true; do
+    res=`curl -s -H "Content-Type: application/json" -X POST --data \
     '{"id":1338,"jsonrpc":"2.0","method":"eth_getCode","params":["0xeec918d74c746167564401103096d45bbd494b74", "latest"]}' \
-    http://docker.for.mac.host.internal:8545 1> /dev/null;
-do
-    >&2 echo -n "."
+    http://docker.for.mac.host.internal:8545 | jq .result -r | wc -m`
+    echo $res
+    # length larger than 0x0
+    [[ $res -gt 3 ]] && break
     sleep 1
 done
 
 echo "contract has been deployed, operater is started."
-
-exec $cmd
+echo $cmd
+$cmd
