7e565db74a30910cfbdffae3081782a7a5936939
wshino <wshinohara@gmail.com>
2019-04-12 12:17:43 +0900

    remove debug code

--- wait.sh
+++ wait.sh
@@ -10,7 +10,7 @@ while true; do
     res=`curl -s -H "Content-Type: application/json" -X POST --data \
     '{"id":1338,"jsonrpc":"2.0","method":"eth_getCode","params":["0xeec918d74c746167564401103096d45bbd494b74", "latest"]}' \
     ${ROOTCHAIN_ENDPOINT} | jq .result -r | wc -m`
-    echo $res
+
     # length larger than 0x0
     [[ $res -gt 3 ]] && break
     sleep 1
